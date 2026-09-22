53ba4cf1340b9273bd2e63c0520a2941dd7f258c
Chenyan Zhao <cz3000@columbia.edu>
2026-09-23 06:00:21 +0800

    Connect app to Supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,36 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Hello World!</h1>
-      <p>My first Next.js website.</p>
-    </main>
-  );
+import { createClient } from "@supabase/supabase-js";
+
+export default async function Home() {
+    const supabase = createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+    );
+
+    const { data: movies, error } = await supabase
+        .from("movies")
+        .select("*")
+        .order("id");
+
+    if (error) {
+        return (
+            <main>
+                <h1>Movies</h1>
+                <p>Error loading movies: {error.message}</p>
+            </main>
+        );
+    }
+
+    return (
+        <main>
+            <h1>My Movie List</h1>
+
+            <ul>
+                {movies?.map((movie) => (
+                    <li key={movie.id}>
+                        <strong>{movie.title}</strong> — {movie.genre}
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
 }
